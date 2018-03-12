b6001361fcf87702cd8a3cbc0256cd2bc454a6f1
maheshyadav007 <maheshyadavindian@gmail.com>
2018-03-12 10:50:29 +0530

    final

--- news.py
+++ news.py
@@ -43,7 +43,7 @@ def webhook():
 	res = processRequest(req)
 
 	res = json.dumps(res, indent=4)
-	# print(res)
+	print(res)
 	#res="{"ffe":"edee"}"
 	r = make_response(res)
 	r.headers['Content-Type'] = 'application/json'
